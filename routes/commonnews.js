76b555666e330b6b1591ba4abd4e6fd0d2c30344
Anub12 <anubhavgahlawatcool01@gmail.com>
2022-11-08 13:35:59 +0530

    flagged commit

--- routes/commonnews.js
+++ routes/commonnews.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
     // const url = 'https://newsapi.org/v2/everything?q=tesla&from=2022-10-07&sortBy=publishedAt&apiKey=3cd636ab8c364bc48cc5d258a461b998'
 
     const url = `https://newsdata.io/api/1/news?apikey=pub_13243050292cfdf6cfca26f59ecef5ef2baaa&language=en`
-    
+
     request({ url, json: true }, (error, response, body) => {
         if (error) {
             return console.log(error);
@@ -18,11 +18,12 @@ router.get('/', (req, res) => {
         // console.log(body.status)
         // console.log(body.message)
         console.log(body)
-        res.render('index', {body: body});
+        res.render('index', {
+            body: body
+        });
         // res.send(body.link)
     })
 
-    
 })
 
 
@@ -31,9 +32,10 @@ router.get('/', (req, res) => {
 // GET signup
 router.get('/getnews', function (req, res) {
     const body = {
-        message : 'active'
+        message: 'active',
+        results: {content: 'abcdef'}
     }
-    res.render('getnews', {body: body});
+    res.render('getnews', { body: body });
 });
 
 
@@ -57,23 +59,40 @@ router.post('/getnews', function (req, res) {
 
     // const url = `https://newsdata.io/api/1/news?apikey=pub_13243050292cfdf6cfca26f59ecef5ef2baaa&language=en`
 
-    const url = `https://newsdata.io/api/1/news?apikey=pub_13243050292cfdf6cfca26f59ecef5ef2baaa&q=${input}&language=en `
+    // const url = `https://newsdata.io/api/1/news?apikey=pub_13243050292cfdf6cfca26f59ecef5ef2baaa&q=${input}&language=en `
 
-    // const api_key = 'pub_13243050292cfdf6cfca26f59ecef5ef2baaa'
-    
-    request({ url, json: true,  }, (error, response, body) => {
+    // // const api_key = 'pub_13243050292cfdf6cfca26f59ecef5ef2baaa'
+
+    // request({ url, json: true,  }, (error, response, body) => {
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+
+    //     console.log(body)
+    //     res.render('getnews', {body: body});
+    // })
+
+    const url = `https://newsdata.io/api/1/news?apikey=pub_13243050292cfdf6cfca26f59ecef5ef2baaa&language=en`
+
+    request({ url, json: true }, (error, response, body) => {
         if (error) {
             return console.log(error);
         }
-        
-        console.log(body)
-        res.render('getnews', {body: body});
-    })
+        // console.log(body.status)
+        // console.log(body.message)
+        // console.log(body)
+
+        var data = body.results[0].content;
+        console.log(data);
+
+        res.render('getnews', {
+            body: body
+        });
 
 
 
-});
-
+    });
+})
 
 
 
